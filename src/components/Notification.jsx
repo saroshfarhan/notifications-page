623f71768bd18361f0ab0270f341e11c634e8088
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-11-26 16:25:56 +0530

    fonts addition from google fonts

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Notifications() {
   return (
     <div className="mx-auto">
-      <h1 className="text-center text-3xl font-semibold text-lGrayBlue-100">
+      <h1 className="text-center text-3xl font-semibold">
         Notifications content
       </h1>
     </div>
