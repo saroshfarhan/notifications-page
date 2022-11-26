c006fc4fecd5c4aab776ebe2c46aefb61d58a194
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-11-26 16:21:41 +0530

    initial project setup

---
+++ src/components/Notification.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Notifications() {
+  return (
+    <div className="mx-auto">
+      <h1 className="text-center text-3xl font-semibold text-lGrayBlue-100">
+        Notifications content
+      </h1>
+    </div>
+  );
+}
+
+export default Notifications;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primRed: "hsl(1, 90%, 64%)",
+        primBlue: "hsl(219, 85%, 26%)",
+        vlGrayBlue: "hsl(210, 60%, 98%)",
+        lGrayBlue: {
+          100: "hsl(211, 68%, 94%)",
+          200: "hsl(205, 33%, 90%)",
+        },
+        grayishBlue: "hsl(219, 14%, 63%)",
+        dGrayBlue: "hsl(219, 12%, 42%)",
+        vdBlue: "hsl(224, 21%, 14%)",
+      },
+    },
+  },
+
+  plugins: [],
+};
